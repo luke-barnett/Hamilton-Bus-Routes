456e53d31a437e22a63b01900883f1506adced42
Luke Barnett <luke@barnett.net.nz>
2012-10-24 10:32:32 +1300

    Interaction!

--- js/application.js
+++ js/application.js
@@ -1,16 +1,18 @@
 function initRoutes(){
-	var _routes = $('path[id|="route"]');
+	var _routes = $('path[class="route"]');
 	_routes.each(function(){
 		$(this).click(function(){
+			//Add any functionality on click here
 			var _item = $(this);
 		});
 	});
 }
 
 function initStops(){
-	var _routes = $('path[id|="stop"]');
-	_routes.each(function(){
+	var _stops = $('use[class="stop"]');
+	_stops.each(function(){
 		$(this).click(function(){
+			//Add any functionality on click here
 			var _item = $(this);
 		});
 	});
